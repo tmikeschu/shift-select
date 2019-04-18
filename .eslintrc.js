71e02e4ab0badc5b8fe757381798c1cc009ed817
Mike Schutte <schutte@clinc.com>
2019-04-18 12:24:40 -0400

    Add eslint and prettier config

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true
+  },
+  extends: ["eslint:recommended", "prettier"],
+  parserOptions: {
+    ecmaVersion: 2018
+  },
+  rules: {}
+};
